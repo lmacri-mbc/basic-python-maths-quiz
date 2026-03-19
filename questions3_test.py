b12ea8c885f6a10a3cb78d7e6e49cb59cb483220
Lee Macri <lee.macri@mbc.school.nz>
2026-03-19 19:21:27 +1300

    Update questions3_test.py

--- questions3_test.py
+++ questions3_test.py
@@ -1,16 +1,17 @@
 # question3_test.py
 import unittest
-import question   # imports student code
-
-class TestQuizQuestions(unittest.TestCase):
-
-    def test_question3(self):
-        self.assertTrue(quiz.question3(21))
-        self.assertTrue(quiz.question3(42))
-        self.assertFalse(quiz.question3(9))
-        self.assertFalse(quiz.question3(14))
-        self.assertFalse(quiz.question3(10))
+#import question   # imports student code
 
 
-if __name__ == "__main__":
-    unittest.main()
+#class TestQuizQuestions(unittest.TestCase):
+
+#    def test_question3(self):
+#       self.assertTrue(quiz.question3(21))
+#       self.assertTrue(quiz.question3(42))
+#       self.assertFalse(quiz.question3(9))
+#       self.assertFalse(quiz.question3(14))
+#       self.assertFalse(quiz.question3(10))
+
+
+#if __name__ == "__main__":
+#    unittest.main()
