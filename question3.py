e7fa49c791cc66e2e6bc25fa6a81d67f7f83aa95
Lee Macri <lee.macri@mbc.school.nz>
2026-03-19 19:06:17 +1300

    Create question3.py

---
+++ question3.py
@@ -0,0 +1,8 @@
+# question3.py
+# Write a program to check if a number is divisible by 3 and by 7
+# When num1 is tested, it should print "True" and when num2 is
+# tested, it should print "False".
+
+num1 = 21
+num2 = 23
+
